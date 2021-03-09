aab37f6271e81e8c1581c66fc7547ee0be046915
Alaa Ibrahim <Alaa4eg@outlook.com>
2021-03-09 16:41:18 +0200

    START jS SLIDER

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const sliderContainer = document.querySelector('.header__slider');
+const sliderElement = document.querySelectorAll('.slider');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let conuter = 1;
+
+// For loop
+
